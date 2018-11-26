4149c93fb677aa69725bb523866ecc14f41f597c
unknown <liushuhao@sendcloud.im>
2018-11-26 17:28:28 +0800

    css

--- src/component/Left.vue
+++ src/component/Left.vue
@@ -1,18 +1,30 @@
 <template>
   <div class="wrapper">
-    <div class="row">
-      <p>【全国】智能网关设备情况</p>
-      <div>网关总量123,456,789</div>
-      <div>维修问题456,789</div>
+    <div class="title">
+      <div class="left">
+        <img src="../common/img/title-left.png" alt="">
+        <p>
+          <img src="../common/img/icon/wifi.png" alt="">
+          <span>【全国】网关情况</span>
+        </p>
+        <img src="../common/img/title-right.png" alt="">
+      </div>
+      <div class="right">
+        <span class="font-small">网关总量</span>
+        <span class="font-color">123,456,789</span>
+      </div>
     </div>
-    <div class="pic">
-      <cirque class="item" :type="1"></cirque>
-      <cirque class="item" :type="2"></cirque>
-      <cirque class="item" :type="3"></cirque>
+    <div class="container">
+      <img src="../common/img/border-left.png" alt="">
+      <div class="cirque-wrapper">
+        <cirque class="item" :type="1"></cirque>
+        <cirque class="item" :type="2"></cirque>
+        <cirque class="item" :type="3"></cirque>
+      </div>
+      <img src="../common/img/border-right.png" alt="">
     </div>
   </div>
 
-  
 </template>
 
 <script type="text/ecmascript-6">
@@ -29,18 +41,38 @@ export default {
 </script>
 
 <style scoped lang="stylus">
+@import '~common/stylus/variable'
   .wrapper 
     display flex
     flex-direction column
-    .row
+    .title
       display flex
-    .pic
+      .left
+        display flex
+        p
+          display flex
+          align-items center
+          padding-right .6rem
+          color #fff
+          background #165AA2
+      .right
+        display flex
+        align-items center
+        .font-small
+          display inline-block
+          margin 0 .2rem
+          font-size $font-small
+        .font-color
+          color $color-active
+    .container
       display flex
       justify-content center
-      border 1px solid #39497B
-      .item
-        width 33%
-        height 30vh
+      .cirque-wrapper
+        border-top 1px solid #3C8BB7
+        border-bottom 1px solid #3C8BB7
+        .item
+          width 33%
+          height 30vh
 </style>
 
 
