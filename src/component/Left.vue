3bf6f07dd2c7c97266f5beef709e5249e212c0de
unknown <liushuhao@sendcloud.im>
2018-11-27 18:02:31 +0800

    ?

--- src/component/Left.vue
+++ src/component/Left.vue
@@ -1,28 +1,84 @@
 <template>
   <div class="wrapper">
-    <div class="title">
-      <div class="left">
-        <img src="../common/img/title-left.png" alt="">
-        <p>
-          <img src="../common/img/icon/wifi.png" alt="">
-          <span>【全国】网关情况</span>
-        </p>
-        <img src="../common/img/title-right.png" alt="">
+    <div class="item">
+      <div class="title">
+        <div class="left">
+          <img src="../common/img/title-left.png" alt="">
+          <p>
+            <img src="../common/img/icon/wifi.png" alt="">
+            <span>【全国】网关情况</span>
+          </p>
+          <img src="../common/img/title-right.png" alt="">
+        </div>
+        <div class="right">
+          <span class="font-small">网关总量</span>
+          <span class="font-color">123,456,789</span>
+        </div>
       </div>
-      <div class="right">
-        <span class="font-small">网关总量</span>
-        <span class="font-color">123,456,789</span>
+      <div class="container">
+        <img src="../common/img/border-left.png" alt="">
+        <div class="cirque-wrapper">
+          <cirque class="item" :type="1"></cirque>
+          <cirque class="item" :type="2"></cirque>
+          <cirque class="item" :type="3"></cirque>
+        </div>
+        <img src="../common/img/border-right.png" alt="">
       </div>
     </div>
-    <div class="container">
-      <img src="../common/img/border-left.png" alt="">
-      <div class="cirque-wrapper">
-        <cirque class="item" :type="1"></cirque>
-        <cirque class="item" :type="2"></cirque>
-        <cirque class="item" :type="3"></cirque>
+
+    <div class="item" style="margin: .6rem 0">
+      <div class="title">
+        <div class="left">
+          <img src="../common/img/title-left.png" alt="">
+          <p>
+            <img src="../common/img/icon/wifi.png" alt="">
+            <span>【全国】智能电表设备情况</span>
+          </p>
+          <img src="../common/img/title-right.png" alt="">
+        </div>
+        <div class="right">
+          <span class="font-small">网关总量</span>
+          <span class="font-color">123,456,789</span>
+        </div>
       </div>
-      <img src="../common/img/border-right.png" alt="">
+      <div class="container">
+        <img src="../common/img/border-left.png" alt="">
+        <div class="cirque-wrapper">
+          <cirque class="item" :type="4"></cirque>
+          <cirque class="item" :type="5"></cirque>
+          <cirque class="item" :type="6"></cirque>
+        </div>
+        <img src="../common/img/border-right.png" alt="">
+      </div>
     </div>
+
+    <div class="item">
+      <div class="title">
+        <div class="left">
+          <img src="../common/img/title-left.png" alt="">
+          <p>
+            <img src="../common/img/icon/wifi.png" alt="">
+            <span>【全国】智能门锁设备情况</span>
+          </p>
+          <img src="../common/img/title-right.png" alt="">
+        </div>
+        <div class="right">
+          <span class="font-small">网关总量</span>
+          <span class="font-color">123,456,789</span>
+        </div>
+      </div>
+      <div class="container">
+        <img src="../common/img/border-left.png" alt="">
+        <div class="cirque-wrapper">
+          <cirque class="item" :type="7"></cirque>
+          <cirque class="item" :type="8"></cirque>
+          <cirque class="item" :type="9"></cirque>
+        </div>
+        <img src="../common/img/border-right.png" alt="">
+      </div>
+    </div>
+
+    
   </div>
 
 </template>
